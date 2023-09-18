f183121ac2a4cf388cb40508a6cbf695fda1110f
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2023-09-19 01:45:51 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,15 @@ const cors = require("cors");
 const app = express();
 app.use(express.json())
 app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://ieee-frontend-wheat.vercel.app", // Replace with your frontend URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // This allows cookies to be included in the request
+  })
+);
+
 const dbconnection= require("./config/DB_Con");
 dbconnection();
 const UserRouter= require("./routes/UserRoutes");
